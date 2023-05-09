5d474126a3d24cb505ab8899d297ecb52b4e4693
egemenc <egemenc2101@gmail.com>
2023-05-09 18:29:57 +0300

    added in our addToCart feature

--- src/components/cart-dropdown/cart-dropdown.components.jsx
+++ src/components/cart-dropdown/cart-dropdown.components.jsx
@@ -1,11 +1,20 @@
-import './cart-dropdown.styles.scss'
-import Button from '../button/button.component'
-const CartDropDown = ()=>{
-    return(
-        <div className='cart-dropdown-container'>
-            <div className='cart-items'/>
-            <Button>GO TO CHECKOUT</Button>
-        </div>
-    )
-}
+import "./cart-dropdown.styles.scss";
+import {useContext} from "react";
+import Button from "../button/button.component";
+import {CartContext} from "../../contexts/cart.contexts";
+import CartItem from "../cart-item/cart-item.component";
+const CartDropDown = () => {
+  const {cartItems} = useContext(CartContext);
+
+  return (
+    <div className="cart-dropdown-container">
+      <div className="cart-items">
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
+        ))}
+      </div>
+      <Button>GO TO CHECKOUT</Button>
+    </div>
+  );
+};
 export default CartDropDown;
--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,12 +4,13 @@ import {ReactComponent as ShoppingItem} from "../assets/shopping-bag.svg";
 import {CartContext} from "../../contexts/cart.contexts";
 
 const CartIcon = () => {
-  const {stateOfCart, setTheStateOfCart} = useContext(CartContext);
+  const {stateOfCart, setTheStateOfCart, cartCount} = useContext(CartContext);
+
   const changeTheStateOfCart = () => setTheStateOfCart(!stateOfCart);
   return (
     <div className="cart-icon-container" onClick={changeTheStateOfCart}>
       <ShoppingItem className="shopping-icon" />
-      <span className="item-count">10</span>
+      <span className="item-count">{cartCount}</span>
     </div>
   );
 };
